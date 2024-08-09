b7d6e6bb370b64ad19de18f1cdb34f33b897b680
miqueljordan <miqueljordan11@gmail.com>
2024-08-09 18:28:41 +0200

    implementar els 3 botons v1.2

--- repartir_quatre.js
+++ repartir_quatre.js
@@ -27,3 +27,54 @@ function calcular_quatre() {
     // Mostrar el resultado en la página
     document.getElementById('resultado1').innerText = printar;
 }
+
+function primera_segona(curts) {
+    console.log('Función primera_segona iniciada');
+
+    var moment = curts - 3;
+    if ((moment + curts + 1) % 4 === 0) {
+        dreta = false;
+        printar += "\n Primera tirada: Comptar " + curts + " .\n Segona tirada: Comptar " + moment + " i dir Un DOS (esquerra) .";
+    } else {
+        dreta = true;
+        printar += "\n Primera tirada: Comptar " + curts + " .\n Segona tirada: Comptar " + moment + " i dir Un DOS (dreta) .";
+    }
+}
+
+function tercera_quarta(llargs) {
+    console.log('Función tercera_quarta iniciada');
+
+    var moment;
+    var moment_actual;
+
+    printar += "\n Tercera tirada: Comptar " + llargs + " .";
+
+    if (llargs % 2 === 0) {
+        moment = llargs - 5;
+        moment_actual = moment;
+        if (dreta === false) {
+            moment_actual += 4;
+        }
+        if ((moment_actual + llargs + 1) % 8 === 0) {
+            printar += "\n Quarta tirada: Comptar " + moment + " i dir Un QUATRE (esquerra) .";
+            dreta = false;
+        } else {
+            printar += "\n Quarta tirada: Comptar " + moment + " i dir Un QUATRE (dreta) .";
+            dreta = true;
+        }
+
+    } else {
+        moment = llargs - 3;
+        moment_actual = moment;
+        if (dreta === false) {
+            moment_actual += 4;
+        }
+        if ((moment_actual + llargs + 1) % 8 === 0) {
+            printar += "\n Quarta tirada: Comptar " + moment + " i dir Un DOS (esquerra) .";
+            dreta = false;
+        } else {
+            printar += "\n Quarta tirada: Comptar " + moment + " i dir Un DOS (dreta) .";
+            dreta = true;
+        }
+    }
+}
